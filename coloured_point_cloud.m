2819a6009a30bae5f488615cee508660c975c5f5
thejerrycheng <qilong.cheng@mail.utoronto.ca>
2022-08-13 16:51:32 -0400

    function for plotting the point cloud with colour graded

--- coloured_point_cloud.m
+++ coloured_point_cloud.m
@@ -2,46 +2,37 @@ function coloured_point_cloud(range_x, range_y, error)
 
 num_points = size(error);
 
-for i = 1:num_points
-
-    if error < 0.05
-
-
-        
-    elseif error < 0.1
-
-
-
-    elseif error < 0.5
-
-
-
-    elseif error < 1
-
-
-
-    elseif error < 2
-
-
-
-    elseif error < 5
-
-
-
-    else 
-
-
-    end 
-
-end 
-
-
+[sortedError sortIndexes] = sort(error);
+range_xs = range_x(sortIndexes);
+range_ys = range_y(sortIndexes);
 
 figure
+cmap = jet(length(error));
+scatter(range_xs, range_ys, 50, cmap, 'filled')
+grid on;
+title('Points where color depends on error magnitude -- red means more error; blue means less error', ...
+  'FontSize', 10);
+% Enlarge figure to full screen.
+% set(gcf, 'units','normalized','outerposition',[0 0 1 1]);
+% Give a name to the title bar.
+set(gcf,'name','Colour-graded point cloud data based on the error value','numbertitle','off');
+colormap(cmap);
 
 
-
-
+% plot(range_x_1, range_y_1, 'o');
+% hold on;
+% plot(range_x_2, range_y_2, 'o');
+% hold on;
+% plot(range_x_3, range_y_3, 'o');
+% hold on;
+% plot(range_x_4, range_y_4, 'o');
+% hold on;
+% plot(range_x_5, range_y_5, 'o');
+% hold on;
+% plot(range_x_6, range_y_6, 'o');
+% hold on;
+% plot(range_x_7, range_y_7, 'o');
+% hold on;
 
 
 end
